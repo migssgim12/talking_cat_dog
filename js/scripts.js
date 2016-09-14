be376f97f6e6a50dc5adc158a302eef8a64ca4ff
Michael Poehner <michaeljpoehner@gmail.com>
2016-09-14 00:54:15 -0700

    Notice in this one it is just ul tags and prepend method

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
-    $("ul#user").prepend("<li>Hello!</li>");
-    $("ul#webpage").prepend("<li>Why hello there</li>");
-    $("body").css("background-color" , "green");
+    $("ul").prepend("<li>Hello!</li>");
+
 });
   $("button#goodbye").click(function() {
-    $("ul#user").prepend("<li>Goodbye!</li>");
-    $("ul#webpage").prepend("<li>Goodbye dear user</li>");
-    $("body").css("background-color" , "white");
+    $("ul").prepend("<li>Goodbye!</li>");
+
 });
 
   $("button#stop").click(function() {
-    $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
-    $("body").css("background-color" , "white");
+    $("ul").prepend("<li>Stop copying me!</li>");
+
   });
 });
+
+// $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+// $("body").css("background-color" , "white");
+// $("ul#webpage").prepend("<li>Goodbye dear user</li>");
+// $("body").css("background-color" , "white");
+
+// $("ul#webpage").prepend("<li>Why hello there</li>");
+// $("body").css("background-color" , "green");
